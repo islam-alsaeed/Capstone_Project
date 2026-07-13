45bf196da9ca6875c7cef84e66017ab8698acb99
Sam(Islam) <islam.alsaeed@gmail.com>
2026-07-12 20:04:27 -0400

    making home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,19 @@
+import "./Home.css";
 function Home() {
   return (
 
-    <div style={{ display: 'block', marginTop: '80px' }}>
+    <div className="Home-container">
         <img 
          src="logo.png" alt="Logo" 
         style={{ width: '150px', height: 'auto' }}
         />
         
       <h1>Welcome to Facial Recoginition control system </h1>
-      <p>This is the home page of our application.</p>
+      <p>Click button below to start.</p>
+      <button style={{  margin: '30px' ,padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}
+      onClick={() => window.location.href = '/login'}>
+        I am ready start
+        </button>
     </div>
   );
 }

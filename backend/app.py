1d7a3bd298cce5a77f6376a2d4523f3e6d59a5ec
Sam(Islam) <islam.alsaeed@gmail.com>
2026-07-23 15:01:20 -0400

    fixing app.py

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,20 @@
-from routes.camera_routes import camera_bp
-from routes.employee_routes import employee_bp
-
 from flask import Flask, redirect
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
+from routes.employee_routes import employee_bp
+from routes.camera_routes import camera_bp
+from routes.face_routes import face_bp
 
-app.register_blueprint(camera_bp)
-app.register_blueprint(employee_bp)
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+app.register_blueprint(employee_bp, url_prefix="/employee")
+app.register_blueprint(camera_bp, url_prefix="/camera")
+app.register_blueprint(face_bp, url_prefix="/face")
 
 @app.get("/")
 def home():
-    # Redirect to React Dashboard
     return redirect("http://localhost:5173/")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
-

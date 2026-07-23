c1ba37ea6a3f3ce4cc9245a468d2fa089d2b9351
Sam(Islam) <islam.alsaeed@gmail.com>
2026-07-22 22:46:46 -0400

    "work in the welcome page design"

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router";
 import "./Home.css";
+import "./LoginPage.css";
 
 function Home() {
   const navigate = useNavigate();
@@ -12,10 +13,12 @@ function Home() {
       />
 
       <h1>Welcome to Facial Recoginition control system </h1>
-      <p>Click button below to start.</p>
-      <button className="start-button"
-        onClick={() => navigate("/login")}>
-        I am ready start
+      <p className="instruction-text">
+        Click the button below to start
+      </p>
+
+      <button className="btn-primary" onClick={() => navigate("/login")}>
+        I am ready — Start
       </button>
       <p className="footer">
         System Version 1.0.0  &copy; 2026 Facial Recognition Control System. All rights reserved.
